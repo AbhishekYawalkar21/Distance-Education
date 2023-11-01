6299f93183a5b7e1309e1c8f83c92fb0849fdb0d
AbhishekYawalkar21 <abhishekyawalkar4@gmail.com>
2023-10-31 21:05:57 -0400

    Added templates

--- myappF23/urls.py
+++ myappF23/urls.py
@@ -6,5 +6,6 @@ app_name = 'myappF23'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('<category_no>/', views.detail, name='detail')
+    path('<int:category_no>/', views.detail, name='detail'),
+    path('course-description/<int:ins_id>/', views.ins_course_stud, name='ins_course_stud')
 ]
--- myappF23/views.py
+++ myappF23/views.py
@@ -4,38 +4,68 @@ from .models import Category, Course, Student, Instructor
 
 # Create your views here.
 def index(request):
-    category_list = Category.objects.all().order_by('id')[:10]
-    response = HttpResponse()
-    heading1 = '<h2>' + 'List of Categories:' + '</h2>'
-    response.write(heading1)
-    for category in category_list:
-        para = '<li>' + str(category) + '</li>'
-        response.write(para)
+    # with HttpResponse
+    # category_list = Category.objects.all().order_by('id')[:10]
+    # response = HttpResponse()
+    # heading1 = '<h2>' + 'List of Categories:' + '</h2>'
+    # response.write(heading1)
+    # for category in category_list:
+    #     para = '<li>' + str(category) + '</li>'
+    #     response.write(para)
+    #
+    # course_list = Course.objects.all().order_by('-price')[:5]
+    # heading1 = '<h2>' + 'List of Courses:' + '</h2>'
+    # response.write(heading1)
+    # for course in course_list:
+    #     para = '<li>' + str(course) + ': ' + str(course.price) + '</li>'
+    #     response.write(para)
+    # return response
 
-    course_list = Course.objects.all().order_by('-price')[:5]
-    heading1 = '<h2>' + 'List of Courses:' + '</h2>'
-    response.write(heading1)
-    for course in course_list:
-        para = '<li>' + str(course) + '</li>'
-        response.write(para)
-    return response
+    # with render
+    category_list = Category.objects.all().order_by('id')[:10]
+    return render(request, 'myappF23/index0.html', {'category_list': category_list})
 
 def about(request):
-    response = HttpResponse()
-    text1 = '<h1>' + 'This is a Distance Education Website! Search our Categories to find all available Courses.' + '</h1>'
-    response.write(text1)
-    return response
+    # with HttpResponse
+    # response = HttpResponse()
+    # text1 = '<h1>' + 'This is a Distance Education Website! Search our Categories to find all available Courses.' + '</h1>'
+    # response.write(text1)
+    # return response
+
+    # With render
+    return render(request, 'myappF23/about0.html')
 
 def detail(request, category_no):
-    get_object_or_404(Category, id=category_no)
-    category = Category.objects.get(id=category_no)
+    # With Http Response
+    # get_object_or_404(Category, id=category_no)
+    # category = Category.objects.get(id=category_no)
+    # courses = category.course_set.all()
+    # response = HttpResponse()
+    # category_name = '<h1>' + str(category) + '</h1>'
+    # response.write(category_name)
+    # heading2 = '<h2>' + 'Courses:' + '</h2>'
+    # response.write(heading2)
+    # for course in courses:
+    #     list_of_courses = '<li>' + str(course) + '</li>'
+    #     response.write(list_of_courses)
+    # return response
+
+    # With Render
+    category = get_object_or_404(Category, pk=category_no)
     courses = category.course_set.all()
-    response = HttpResponse()
-    category_name = '<h1>' + str(category) + '</h1>'
-    response.write(category_name)
-    heading2 = '<h2>' + 'Courses:' + '</h2>'
-    response.write(heading2)
-    for course in courses:
-        list_of_courses = '<li>' + str(course) + '</li>'
-        response.write(list_of_courses)
-    return response
+    categories_courses = {
+        'category': category,
+        'courses': courses,
+    }
+    return render(request, 'myappF23/detail0.html', {'categories_courses': categories_courses})
+
+def ins_course_stud(request, ins_id):
+    course = Course.objects.get(id=ins_id)
+    instructor = course.instructor.first_name + ' ' + course.instructor.last_name
+    students = course.students.all()
+    context = {
+        'instructor': instructor,
+        'students': students,
+        'course': course,
+    }
+    return render(request, 'myappF23/course-details.html', {'context': context})
